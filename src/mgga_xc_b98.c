49c390612a3f4439e3aea2910cc59002c2ec6aa9
Miguel Marques <marques@tddft.org>
2018-05-23 17:46:36 +0200

    B98 is a hybrid mgga, not a mgga

--- src/mgga_xc_b98.c
+++ src/mgga_xc_b98.c
@@ -9,23 +9,29 @@
 
 #include "util.h"
 
-#define XC_MGGA_XC_B98         598 /* Becke 98 */
+#define XC_HYB_MGGA_XC_B98         598 /* Becke 98 */
+
+static void
+hyb_mgga_xc_b98_init(xc_func_type *p)
+{
+  p->cam_alpha = 0.1985;
+}
 
 #include "maple2c/mgga_xc_b98.c"
 
 #define func maple2c_func
 #include "work_mgga_c.c"
 
-const xc_func_info_type xc_func_info_mgga_xc_b98 = {
-  XC_MGGA_XC_B98,
+const xc_func_info_type xc_func_info_hyb_mgga_xc_b98 = {
+  XC_HYB_MGGA_XC_B98,
   XC_EXCHANGE_CORRELATION,
   "Becke 98",
-  XC_FAMILY_MGGA,
+  XC_FAMILY_HYB_MGGA,
   {&xc_ref_Becke1998_2092, NULL, NULL, NULL, NULL},
   XC_FLAGS_3D | XC_FLAGS_NEEDS_LAPLACIAN | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC,
   1e-23,
   0, NULL, NULL,
-  NULL, NULL,
+  hyb_mgga_xc_b98_init, NULL,
   NULL, NULL, work_mgga_c,
 };
 
