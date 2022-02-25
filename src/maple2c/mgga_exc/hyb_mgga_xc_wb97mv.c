fd30834a14d582f0638d6e3b01e4565386b43341
Miguel Marques <marques@tddft.org>
2022-02-25 08:35:44 +0100

    Added GPU_DEVICE to mgga files

--- src/maple2c/mgga_exc/hyb_mgga_xc_wb97mv.c
+++ src/maple2c/mgga_exc/hyb_mgga_xc_wb97mv.c
@@ -16,7 +16,7 @@
 
 
 #ifndef XC_DONT_COMPILE_EXC
-static inline void
+GPU_DEVICE_FUNCTION static inline void
 func_exc_pol(const xc_func_type *p, size_t ip, const double *rho, const double *sigma, const double *lapl, const double *tau, xc_mgga_out_params *out)
 {
   double t3, t4, t5, t6, t7, t8, t9, t10;
@@ -370,7 +370,7 @@ func_exc_pol(const xc_func_type *p, size_t ip, const double *rho, const double *
 
 
 #ifndef XC_DONT_COMPILE_VXC
-static inline void
+GPU_DEVICE_FUNCTION static inline void
 func_vxc_pol(const xc_func_type *p, size_t ip, const double *rho, const double *sigma, const double *lapl, const double *tau, xc_mgga_out_params *out)
 {
   double t3, t4, t5, t6, t7, t8, t9, t10;
@@ -1358,7 +1358,7 @@ func_vxc_pol(const xc_func_type *p, size_t ip, const double *rho, const double *
 
 
 #ifndef XC_DONT_COMPILE_FXC
-static inline void
+GPU_DEVICE_FUNCTION static inline void
 func_fxc_pol(const xc_func_type *p, size_t ip, const double *rho, const double *sigma, const double *lapl, const double *tau, xc_mgga_out_params *out)
 {
   double t3, t4, t5, t6, t7, t8, t9, t10;
@@ -4230,7 +4230,7 @@ func_fxc_pol(const xc_func_type *p, size_t ip, const double *rho, const double *
 
 
 #ifndef XC_DONT_COMPILE_KXC
-static inline void
+GPU_DEVICE_FUNCTION static inline void
 func_kxc_pol(const xc_func_type *p, size_t ip, const double *rho, const double *sigma, const double *lapl, const double *tau, xc_mgga_out_params *out)
 {
   double t3, t4, t5, t6, t7, t8, t9, t10;
@@ -12528,7 +12528,7 @@ func_kxc_pol(const xc_func_type *p, size_t ip, const double *rho, const double *
 
 
 #ifndef XC_DONT_COMPILE_LXC
-static inline void
+GPU_DEVICE_FUNCTION static inline void
 func_lxc_pol(const xc_func_type *p, size_t ip, const double *rho, const double *sigma, const double *lapl, const double *tau, xc_mgga_out_params *out)
 {
   double t3, t4, t5, t6, t7, t8, t9, t10;
@@ -32550,7 +32550,7 @@ func_lxc_pol(const xc_func_type *p, size_t ip, const double *rho, const double *
 
 
 #ifndef XC_DONT_COMPILE_EXC
-static inline void
+GPU_DEVICE_FUNCTION static inline void
 func_exc_unpol(const xc_func_type *p, size_t ip, const double *rho, const double *sigma, const double *lapl, const double *tau, xc_mgga_out_params *out)
 {
   double t4, t5, t6, t7, t8, t9, t10, t11;
@@ -32760,7 +32760,7 @@ func_exc_unpol(const xc_func_type *p, size_t ip, const double *rho, const double
 
 
 #ifndef XC_DONT_COMPILE_VXC
-static inline void
+GPU_DEVICE_FUNCTION static inline void
 func_vxc_unpol(const xc_func_type *p, size_t ip, const double *rho, const double *sigma, const double *lapl, const double *tau, xc_mgga_out_params *out)
 {
   double t4, t5, t6, t7, t8, t9, t10, t11;
@@ -33218,7 +33218,7 @@ func_vxc_unpol(const xc_func_type *p, size_t ip, const double *rho, const double
 
 
 #ifndef XC_DONT_COMPILE_FXC
-static inline void
+GPU_DEVICE_FUNCTION static inline void
 func_fxc_unpol(const xc_func_type *p, size_t ip, const double *rho, const double *sigma, const double *lapl, const double *tau, xc_mgga_out_params *out)
 {
   double t4, t5, t6, t7, t8, t9, t10, t11;
@@ -34142,7 +34142,7 @@ func_fxc_unpol(const xc_func_type *p, size_t ip, const double *rho, const double
 
 
 #ifndef XC_DONT_COMPILE_KXC
-static inline void
+GPU_DEVICE_FUNCTION static inline void
 func_kxc_unpol(const xc_func_type *p, size_t ip, const double *rho, const double *sigma, const double *lapl, const double *tau, xc_mgga_out_params *out)
 {
   double t4, t5, t6, t7, t8, t9, t10, t11;
@@ -35918,7 +35918,7 @@ func_kxc_unpol(const xc_func_type *p, size_t ip, const double *rho, const double
 
 
 #ifndef XC_DONT_COMPILE_LXC
-static inline void
+GPU_DEVICE_FUNCTION static inline void
 func_lxc_unpol(const xc_func_type *p, size_t ip, const double *rho, const double *sigma, const double *lapl, const double *tau, xc_mgga_out_params *out)
 {
   double t4, t5, t6, t7, t8, t9, t10, t11;
